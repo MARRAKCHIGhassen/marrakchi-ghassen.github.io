fe42df9d1f0191714461451d8dc6ff43971fdd97
MARRAKCHIGhassen <43677888+MARRAKCHIGhassen@users.noreply.github.com>
2026-05-28 16:35:01 +0000

    Deploying to gh-pages from @ MARRAKCHIGhassen/marrakchi-ghassen.github.io@3b5d2635b2291e3db538c2a28c4cf460d5e304c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -542,6 +542,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/marrakchi-ghassen", "_blank");
         },
       },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.researchgate.net/profile/Ghassen-Marrakchi", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',

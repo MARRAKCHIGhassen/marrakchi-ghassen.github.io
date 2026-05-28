25800612c8bf1a7db1c935e6d6680060508f1811
MARRAKCHIGhassen <43677888+MARRAKCHIGhassen@users.noreply.github.com>
2026-05-28 18:21:59 +0000

    Deploying to gh-pages from @ MARRAKCHIGhassen/marrakchighassen.github.io@d73eb1d50af8ea47bf9f893812a9b641d1645146 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "MARRAKCHIGhassen/marrakchi-ghassen.github.io",
+    "data-repo": "MARRAKCHIGhassen/marrakchighassen.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
